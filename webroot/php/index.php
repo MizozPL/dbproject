01a1e678f3922ec6626aaee0d8c36198241b7c8d
MizozPL <dox44@wp.pl>
2022-01-19 22:30:50 +0100

    fix

--- webroot/php/index.php
+++ webroot/php/index.php
@@ -2,34 +2,6 @@
 require_once "./config/userLevel.php";
 
 session_start();
-?>
-<!doctype html>
-
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>System obsługi rachunków</title>
-</head>
-
-<body>
-<div class="container">
-    <form method="post" action="">
-        <div id="div_login">
-            <h1>Login</h1>
-            <div>
-                <input type="text" class="textbox" name="txt_uname" placeholder="Username" />
-            </div>
-            <div>
-                <input type="password" class="textbox" name="txt_pwd" placeholder="Password"/>
-            </div>
-            <div>
-                <input type="submit" value="Submit" name="button_submit"/>
-            </div>
-        </div>
-    </form>
-</div>
-
-<?php
 
 if(isset($_SESSION["uname"]) && isset($_SESSION["uprawnienia"])) {
     if ($_SESSION['uprawnienia'] == 'sprzedawca'){
@@ -83,5 +55,30 @@ if(isset($_POST['button_submit'])){
     }
 
 } ?>
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>System obsługi rachunków</title>
+</head>
+
+<body>
+<div class="container">
+    <form method="post" action="">
+        <div id="div_login">
+            <h1>Login</h1>
+            <div>
+                <input type="text" class="textbox" name="txt_uname" placeholder="Username" />
+            </div>
+            <div>
+                <input type="password" class="textbox" name="txt_pwd" placeholder="Password"/>
+            </div>
+            <div>
+                <input type="submit" value="Submit" name="button_submit"/>
+            </div>
+        </div>
+    </form>
+</div>
 </body>
 </html>
