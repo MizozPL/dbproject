ceeb459e9c30b3e5ff162694a5e07d5094cc429e
Kamil Włodarski <261739@student.pwr.edu.pl>
2022-01-19 22:39:55 +0100

    a

--- webroot/php/index.php
+++ webroot/php/index.php
@@ -3,22 +3,22 @@ require_once "./config/userLevel.php";
 
 session_start();
 
-if(isset($_SESSION["uname"]) && isset($_SESSION["uprawnienia"])) {
-    if ($_SESSION['uprawnienia'] == 'sprzedawca'){
+if (isset($_SESSION["uname"]) && isset($_SESSION["uprawnienia"])) {
+    if ($_SESSION['uprawnienia'] == 'sprzedawca') {
         header('Location: sprzedawcaIndex.php');
-    } elseif ($_SESSION['uprawnienia'] == 'menadzer'){
+    } elseif ($_SESSION['uprawnienia'] == 'menadzer') {
         header('Location: menadzerIndex.php');
     } elseif ($_SESSION['uprawnienia'] == 'administrator') {
         header('Location: administratorIndex.php');
     }
 }
 
-if(isset($_POST['button_submit'])){
+if (isset($_POST['button_submit'])) {
 
     $uname = $_POST['txt_uname'];
     $password = $_POST['txt_pwd'];
 
-    if ($uname != "" && $password != ""){
+    if ($uname != "" && $password != "") {
 
         $sql = "call zweryfikujHaslo(?);";
 
@@ -32,7 +32,7 @@ if(isset($_POST['button_submit'])){
                 $return = $result->fetch_assoc();
 
                 //Weryfikacja hasła
-                if(password_verify($password, $return["passwordHash"])) {
+                if (password_verify($password, $return["passwordHash"])) {
                     $_SESSION['uname'] = $uname;
                     $_SESSION['uprawnienia'] = $return["permissionLevel"];
                     if ($_SESSION['uprawnienia'] == 'sprzedawca') {
@@ -43,13 +43,13 @@ if(isset($_POST['button_submit'])){
                         header('Location: administratorIndex.php');
                     }
                 } else {
-                    echo "Złe hasło/login";
+                    $_SESSION["returnMessageString"] = "Złe hasło/login";
                 }
-            }else {
-                echo "Złe hasło/login";
+            } else {
+                $_SESSION["returnMessageString"] = "Złe hasło/login";
             }
         } catch (mysqli_sql_exception $e) {
-            echo "Złe hasło/login";
+            $_SESSION["returnMessageString"] = "Złe hasło/login";
         }
     }
 
@@ -57,27 +57,27 @@ if(isset($_POST['button_submit'])){
 <!doctype html>
 
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>System obsługi rachunków</title>
-</head>
+	<head>
+		<meta charset="utf-8">
+		<title>System obsługi rachunków</title>
+	</head>
 
-<body>
-<div class="container">
-    <form method="post" action="">
-        <div id="div_login">
-            <h1>Login</h1>
-            <div>
-                <input type="text" class="textbox" name="txt_uname" placeholder="Username" />
-            </div>
-            <div>
-                <input type="password" class="textbox" name="txt_pwd" placeholder="Password"/>
-            </div>
-            <div>
-                <input type="submit" value="Submit" name="button_submit"/>
-            </div>
-        </div>
-    </form>
-</div>
-</body>
+	<body>
+		<form method="post" action="">
+			<h1>Login</h1>
+			<label for="nazwaUzytkownika">Nazwa Użytkownika:</label><br>
+			<input id="nazwaUzytkownika" type="text" class="textbox" name="txt_uname" placeholder="Username"/><br>
+			<label for="haslo">Hasło:</label><br>
+			<input id="haslo" type="password" class="textbox" name="txt_pwd" placeholder="Password"/><br>
+			<input type="submit" value="Submit" name="button_submit"/>
+		</form>
+		<p>
+            <?php
+            if (isset($_SESSION["returnMessageString"])) {
+                echo $_SESSION["returnMessageString"];
+                unset($_SESSION["returnMessageString"]);
+            }
+            ?>
+		</p>
+	</body>
 </html>
