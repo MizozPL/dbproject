ed70cce67b105fb522818942a8ba74a974eb8e41
Kamil Włodarski <261739@student.pwr.edu.pl>
2022-01-19 13:06:24 +0100

    dodano sqlowych użytkowników

--- php/index.php
+++ php/index.php
@@ -1,14 +1,63 @@
-<?php
+<!doctype html>
 
-?>
-
-<html>
+<html lang="en">
 <head>
-    <title>
-
-    </title>
+    <meta charset="utf-8">
+    <title>System obsługi rachunków</title>
 </head>
-<body>
 
+<body>
+<div class="container">
+    <form method="post" action="">
+        <div id="div_login">
+            <h1>Login</h1>
+            <div>
+                <input type="text" class="textbox" name="txt_uname" placeholder="Username" />
+            </div>
+            <div>
+                <input type="password" class="textbox" name="txt_pwd" placeholder="Password"/>
+            </div>
+            <div>
+                <input type="submit" value="Submit" name="button_submit"/>
+            </div>
+        </div>
+    </form>
+</div>
+
+<?php
+include "./config/userLevel.php";
+
+if(isset($_POST['button_submit'])){
+
+    $uname = mysqli_real_escape_string($conn,$_POST['txt_uname']);
+    $password = mysqli_real_escape_string($conn,$_POST['txt_pwd']);
+
+    if ($uname != "" && $password != ""){
+
+        $sql = "call zweryfikujHaslo(?, ?);";
+
+        try {
+            $stmt = $conn->prepare($sql); //podkreśla, ale git
+            $stmt->bind_param("ss", $uname, $password);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            if ($result->num_rows == 1) {
+                $uprawnienia =  $result->fetch_assoc()["returnValue"];
+            } else {
+                echo "Złe hasło/login";
+            }
+        } catch (mysqli_sql_exception $e) {
+            echo "Złe hasło/login";
+        }
+
+        if(isset($uprawnienia)){
+            $_SESSION['uname'] = $uname;
+            $_SESSION['uprawnienia'] = $uprawnienia;
+            header('Location: home.php');
+        }
+    }
+
+} ?>
 </body>
 </html>
---
+++ sql/initializeRoles.sql
@@ -0,0 +1,67 @@
+USE db_project;
+
+CREATE USER 'sprzedawca'@'localhost';
+
+GRANT EXECUTE ON PROCEDURE dodajPrzedmiot TO 'sprzedawca'@'localhost';
+GRANT EXECUTE ON PROCEDURE dodajPozycje TO 'sprzedawca'@'localhost';
+GRANT EXECUTE ON PROCEDURE dodajRachunek TO 'sprzedawca'@'localhost';
+GRANT EXECUTE ON PROCEDURE dodajPozycjeDoRachunku TO 'sprzedawca'@'localhost';
+GRANT EXECUTE ON PROCEDURE wypiszPozycje TO 'sprzedawca'@'localhost';
+GRANT EXECUTE ON PROCEDURE wypiszPrzedmiot TO 'sprzedawca'@'localhost';
+GRANT EXECUTE ON PROCEDURE wypiszRachunek TO 'sprzedawca'@'localhost';
+GRANT EXECUTE ON PROCEDURE zweryfikujHaslo TO 'sprzedawca'@'localhost';
+GRANT EXECUTE ON PROCEDURE logujDane TO 'sprzedawca'@'localhost';
+GRANT SELECT ON TABLE przedmioty TO 'sprzedawca'@'localhost';
+GRANT SELECT ON TABLE pozycje TO 'sprzedawca'@'localhost';
+GRANT SELECT ON TABLE rachunki TO 'sprzedawca'@'localhost';
+GRANT SELECT ON TABLE rachunki_pozycje TO 'sprzedawca'@'localhost';
+
+CREATE USER 'menadzer'@'localhost';
+
+GRANT EXECUTE ON PROCEDURE dodajPrzedmiot TO 'menadzer'@'localhost';
+GRANT EXECUTE ON PROCEDURE dodajPozycje TO 'menadzer'@'localhost';
+GRANT EXECUTE ON PROCEDURE dodajRachunek TO 'menadzer'@'localhost';
+GRANT EXECUTE ON PROCEDURE dodajPozycjeDoRachunku TO 'menadzer'@'localhost';
+GRANT EXECUTE ON PROCEDURE wypiszPozycje TO 'menadzer'@'localhost';
+GRANT EXECUTE ON PROCEDURE wypiszPrzedmiot TO 'menadzer'@'localhost';
+GRANT EXECUTE ON PROCEDURE wypiszRachunek TO 'menadzer'@'localhost';
+GRANT EXECUTE ON PROCEDURE zweryfikujHaslo TO 'menadzer'@'localhost';
+GRANT EXECUTE ON PROCEDURE logujDane TO 'menadzer'@'localhost';
+GRANT EXECUTE ON PROCEDURE usunPrzedmiot TO 'menadzer'@'localhost';
+GRANT EXECUTE ON PROCEDURE edytujPrzedmiot TO 'menadzer'@'localhost';
+GRANT EXECUTE ON PROCEDURE usunPozycje TO 'menadzer'@'localhost';
+GRANT EXECUTE ON PROCEDURE edytujPozycje TO 'menadzer'@'localhost';
+GRANT EXECUTE ON PROCEDURE usunRachunek TO 'menadzer'@'localhost';
+GRANT EXECUTE ON PROCEDURE usunPozycjeZRachunku TO 'menadzer'@'localhost';
+GRANT SELECT ON TABLE przedmioty TO 'menadzer'@'localhost';
+GRANT SELECT ON TABLE pozycje TO 'menadzer'@'localhost';
+GRANT SELECT ON TABLE rachunki TO 'menadzer'@'localhost';
+GRANT SELECT ON TABLE rachunki_pozycje TO 'menadzer'@'localhost';
+
+CREATE USER 'administrator'@'localhost';
+
+GRANT EXECUTE ON PROCEDURE dodajPrzedmiot TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE dodajPozycje TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE dodajRachunek TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE dodajPozycjeDoRachunku TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE wypiszPozycje TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE wypiszPrzedmiot TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE wypiszRachunek TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE zweryfikujHaslo TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE logujDane TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE usunPrzedmiot TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE edytujPrzedmiot TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE usunPozycje TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE edytujPozycje TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE usunRachunek TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE usunPozycjeZRachunku TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE dodajUzytkownika TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE usunUzytkownika TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE edytujUzytkownika TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE listujUzytkownikow TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE wyczyscLogi TO 'administrator'@'localhost';
+GRANT EXECUTE ON PROCEDURE wypiszLogi TO 'administrator'@'localhost';
+GRANT SELECT ON TABLE przedmioty TO 'administrator'@'localhost';
+GRANT SELECT ON TABLE pozycje TO 'administrator'@'localhost';
+GRANT SELECT ON TABLE rachunki TO 'administrator'@'localhost';
+GRANT SELECT ON TABLE rachunki_pozycje TO 'administrator'@'localhost';
