35dc1f29f9ce6b22a43171dacbd19c2321b1c038
Kamil Włodarski <261739@student.pwr.edu.pl>
2022-01-19 18:41:04 +0100

    poprawiony system administartora

--- php/administratorMenu/dodajUzytkownikaMenu.php
+++ php/administratorMenu/dodajUzytkownikaMenu.php
@@ -1,80 +1,81 @@
 <?php
 session_start();
 
-if(!isset($_SESSION['uname'])){
+if (!isset($_SESSION['uname'])) {
     header('Location: ../index.php');
 }
 
-if($_SESSION['uprawnienia'] != 'administrator'){
+if ($_SESSION['uprawnienia'] != 'administrator') {
     header('Location: ../brakUprawnien.php');
 }
 
-include "../config/adminLevel.php";
+if (isset($_POST['button_dodajUzytkownika']) && isset($_POST['txt_uzytkownik']) && isset($_POST['txt_haslo']) && isset($_POST['uprawnienia'])) {
 
-$username = $_SESSION['uname'];
+    require_once "../config/adminLevel.php";
 
-if(isset($_POST['button_powrot'])){
-    header('Location: ../administratorIndex.php');
+    $uzytkownik = $_POST['txt_uzytkownik'];
+    $pass = $_POST['txt_haslo'];
+    $poziom_uprawnien = $_POST['uprawnienia'];
+
+    $sql = "call dodajUzytkownika(?, ?, ?);";
+    try {
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("sss", $uzytkownik, $pass, $poziom_uprawnien);
+        $stmt->execute();
+
+        $_SESSION["returnMessageString"] = "Dodano użytkownika: " . $uzytkownik . "<br>";
+
+        $stmt->close();
+        $log = "Dodano użytkownika: " . $uzytkownik;
+        $sql = "call logujDane(?, ?);";
+        try {
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("ss", $_SESSION['uname'], $log);
+            $stmt->execute();
+        } catch (mysqli_sql_exception $e) {
+            $_SESSION["returnMessageString"] = $_SESSION["returnMessageString"] . "Błąd logowania!";
+        }
+
+
+    } catch (mysqli_sql_exception $e) {
+        $_SESSION["returnMessageString"] = "Nie dodano/Błąd";
+    }
 }
 ?>
 
 <!doctype html>
 <html>
-<head>
-    <title>Dodaj użytkownika</title>
-</head>
-<body>
-<h1>Dodaj użytkownika.</h1>
-<form method='post' action="">
-
-    <input type="text" name="txt_uzytkownik" placeholder="Nazwa użytkownika" /><br>
-    <input type="text" name="txt_haslo" placeholder="Hasło" /><br>
-    <label for="uprawnienia">Uprawnienia:</label>
-    <select id="uprawnienia" name="uprawnienia">
-        <option value="sprzedawca">sprzedawca</option>
-        <option value="menadzer">menadżer</option>
-        <option value="administrator">administrator</option>
-    </select><br>
-    <input type="submit" value="zatwierdź" name="button_dodajUzytkownika">
-    <input type="submit" value="powróć" name="button_powrot">
-
-</form>
-<p>
-    <?php
-    if(isset($_POST['button_dodajUzytkownika'])){
-
-        $uzytkownik = $_POST['txt_uzytkownik'];
-        $pass = $_POST['txt_haslo'];
-        $poziom_uprawnien = $_POST['uprawnienia'];
-
-        $sql = "call dodajUzytkownika(?, ?, ?);";
-        try {
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("sss", $uzytkownik, $pass, $poziom_uprawnien);
-            $stmt->execute();
-
-            echo "Dodano<br>";
-
-            $log = "Dodano użytkownika: ".$uzytkownik;
-
-            $sql = "call logujDane(?, ?);";
-            try {
-                $stmt = $conn->prepare($sql);
-                $stmt->bind_param("ss", $username, $log);
-                $stmt->execute();
-            } catch (mysqli_sql_exception $e) {
-                echo "Błąd logowania!!!";
+	<head>
+		<title>Dodaj użytkownika</title>
+	</head>
+	<body>
+		<h1>Dodaj użytkownika.</h1>
+		<form method='post'>
+			<label for="nazwa_uzytkownika">Nazwa użytkownika:</label><br>
+			<input id="nazwa_uzytkownika" required="required" type="text" name="txt_uzytkownik"
+			       placeholder="Nazwa użytkownika"/><br>
+			<label for="haslo">Hasło:</label><br>
+			<input id="haslo" required="required" type="text" name="txt_haslo" placeholder="Hasło"/><br>
+			<label for="uprawnienia">Uprawnienia:</label>
+			<select id="uprawnienia" name="uprawnienia">
+				<option value="sprzedawca">sprzedawca</option>
+				<option value="menadzer">menadżer</option>
+				<option value="administrator">administrator</option>
+			</select><br>
+			<input type="submit" value="zatwierdź" name="button_dodajUzytkownika">
+		</form>
+		<p>
+            <?php
+            if (isset($_SESSION["returnMessageString"])) {
+                echo $_SESSION["returnMessageString"];
+                unset($_SESSION["returnMessageString"]);
             }
-
-
-        } catch (mysqli_sql_exception $e) {
-            echo "Nie dodano/Błąd";
-        }
-    }
-    ?>
-</p>
-
-</body>
+            ?>
+		</p>
+		<form action="../index.php">
+			<input type="submit" value="Powróć"/>
+		</form>
+	</body>
 </html>
 
 
--- php/administratorMenu/wypiszLogiMenu.php
+++ php/administratorMenu/wypiszLogiMenu.php
@@ -1,50 +1,49 @@
 <?php
 session_start();
 
-if(!isset($_SESSION['uname'])){
+if (!isset($_SESSION['uname'])) {
     header('Location: ../index.php');
 }
 
-if($_SESSION['uprawnienia'] != 'administrator'){
+if ($_SESSION['uprawnienia'] != 'administrator') {
     header('Location: ../brakUprawnien.php');
 }
 
-include "../config/adminLevel.php";
+require_once "../config/adminLevel.php";
 
-$username = $_SESSION['uname'];
+$_SESSION["returnMessageString"] = "";
 
-if(isset($_POST['button_powrot'])){
-    header('Location: ../administratorIndex.php');
+$sql = "call wypiszLogi();";
+try {
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $_SESSION["returnMessageString"] = $_SESSION["returnMessageString"] . "id: " . $row["id"] . " - data: " . $row["data"] . " uzytkownik: " . $row["uzytkownik"] . " opis_akcji:" . $row["opis_akcji"] . "<br>";
+        }
+    } else {
+        $_SESSION["returnMessageString"] = "0 wyników";
+    }
+} catch (mysqli_sql_exception $e) {
+    $_SESSION["returnMessageString"] = "Błąd";
 }
+
 ?>
 
 <!doctype html>
 <html>
-<head>
-    <title>Logi</title>
-</head>
-<body>
-<h1>Logi:</h1>
-<form method='post' action="">
-    <input type="submit" value="powróć" name="button_powrot">
-</form>
-<p>
-    <?php
-    $sql = "call wypiszLogi();";
-    try {
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                echo "id: " . $row["id"]. " - data: " . $row["data"]. " uzytkownik: " . $row["uzytkownik"]. " opis_akcji:". $row["opis_akcji"] ."<br>";
-            }
-        } else {
-            echo "0 wyników";
-        }
-    } catch (mysqli_sql_exception $e) {
-        echo "Błąd";
-    }
-    ?>
-</p>
-
-</body>
+	<head>
+		<title>Logi</title>
+	</head>
+	<body>
+		<h1>Logi:</h1>
+		<form method='post' action="../index.php">
+			<input type="submit" value="powróć">
+		</form>
+		<p>
+            <?php
+            echo $_SESSION["returnMessageString"];
+            unset($_SESSION["returnMessageString"]);
+            ?>
+		</p>
+	</body>
 </html>
